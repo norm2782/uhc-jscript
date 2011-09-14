7b52cb7189f5ca2487817febd93febfe0e59f487
Jurriën Stutterheim <j.stutterheim@me.com>
2011-09-14 10:06:15 +0200

    Add first example

---
+++ tests/fold.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+main = do
+  let answ = fldToN (f 3) 10
+  return ()
+
+f :: Int -> Int -> Int -> Int
+f n x xs = n + x + xs
+
+fldToN :: (Int -> Int -> Int) -> Int -> Int
+fldToN f n = foldr f 0 [1..n]
+
+foreign export jscript "fldToN" fldToN :: (Int -> Int -> Int) -> Int -> Int
