9babc4b13e984c8c71409ffb7588ab7fd013cec1
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-17 13:16:09 +0200

    Use toJS instead

--- uhc-jscript/src/Language/UHC/JScript/Primitives.hs
+++ uhc-jscript/src/Language/UHC/JScript/Primitives.hs
@@ -1,6 +1,7 @@
 module Language.UHC.JScript.Primitives where
 
 import Language.UHC.JScript.ECMA.String
+import Language.UHC.JScript.Types
 
 
 
@@ -14,70 +15,70 @@ foreign import prim "primMkAnonObj"
 
 
 mkObj :: String -> IO (JSPtr p)
-mkObj = _mkObj . stringToJSString
+mkObj = _mkObj . toJS
 
 foreign import prim "primMkObj"
   _mkObj :: JSString -> IO (JSPtr p)
 
 mkCtor :: String -> IO (JSFunPtr a)
-mkCtor = _mkCtor . stringToJSString
+mkCtor = _mkCtor . toJS
 
 foreign import prim "primMkCtor"
   _mkCtor :: JSString -> IO (JSFunPtr a)
 
 getCtor :: String -> IO (JSFunPtr a)
-getCtor s1 = _getCtor (stringToJSString s1)
+getCtor s1 = _getCtor (toJS s1)
 
 foreign import prim "primGetCtor"
   _getCtor :: JSString -> IO (JSFunPtr a)
 
 setCtor :: String -> JSFunPtr a -> IO ()
-setCtor s1 fp = _setCtor (stringToJSString s1) fp
+setCtor s1 fp = _setCtor (toJS s1) fp
 
 foreign import prim "primSetCtor"
   _setCtor :: JSString -> JSFunPtr a -> IO ()
 
 getAttr :: String -> JSPtr p -> IO a
-getAttr s p = _getAttr (stringToJSString s) p
+getAttr s p = _getAttr (toJS s) p
 
 foreign import prim "primGetAttr"
   _getAttr :: JSString -> JSPtr p -> IO a
 
 
 setAttr :: String -> a -> JSPtr p -> IO (JSPtr p)
-setAttr s a p = _setAttr (stringToJSString s) a p
+setAttr s a p = _setAttr (toJS s) a p
 
 foreign import prim "primSetAttr"
   _setAttr :: JSString -> a -> JSPtr p -> IO (JSPtr p)
 
 
 modAttr :: String -> (a -> b) -> JSPtr p -> IO (JSPtr p)
-modAttr s f p = _modAttr (stringToJSString s) f p
+modAttr s f p = _modAttr (toJS s) f p
 
 foreign import prim "primModAttr"
   _modAttr :: JSString -> (a -> b) -> JSPtr p -> IO (JSPtr p)
 
 
 getProtoAttr :: String -> String -> IO a
-getProtoAttr x y = _getProtoAttr (stringToJSString x) (stringToJSString y)
+getProtoAttr x y = _getProtoAttr (toJS x) (toJS y)
 
 foreign import prim "primGetProtoAttr"
   _getProtoAttr :: JSString -> JSString -> IO a
 
 
 setProtoAttr :: String -> a -> String -> IO ()
-setProtoAttr x a y = _setProtoAttr (stringToJSString x) a (stringToJSString y)
+setProtoAttr x a y = _setProtoAttr (toJS x) a (toJS y)
 
 foreign import prim "primSetProtoAttr"
   _setProtoAttr :: JSString -> a -> JSString -> IO ()
 
 
 modProtoAttr :: String -> (a -> b) -> String -> IO ()
-modProtoAttr x f y = _modProtoAttr (stringToJSString x) f (stringToJSString y)
+modProtoAttr x f y = _modProtoAttr (toJS x) f (toJS y)
 
 foreign import prim "primModProtoAttr"
   _modProtoAttr :: JSString -> (a -> b) -> JSString -> IO ()
 
 
 foreign import prim "primStringToPackedString"
-  stringToJSString :: String -> JSString
+  toJS :: String -> JSString
