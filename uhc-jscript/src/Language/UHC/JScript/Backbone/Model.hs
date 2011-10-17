24dd7749ddf92445435ef5361cf46fba5835cf88
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-17 14:19:39 +0200

    More backbone functions. Introduce BBModel type

--- uhc-jscript/src/Language/UHC/JScript/Backbone/Model.hs
+++ uhc-jscript/src/Language/UHC/JScript/Backbone/Model.hs
@@ -5,24 +5,79 @@ import Language.UHC.JScript.ECMA.String
 import Language.UHC.JScript.Types
 
 
+data BBModel a
+
 foreign import jscript "Backbone.Model.extend(%*)"
   extend :: JSFunPtr a -> IO (JSFunPtr b)
 
-get :: JSPtr p -> String -> IO a
+get :: JSPtr (BBModel a) -> String -> IO b
 get p s = _get p (toJS s)
 
 foreign import jscript "%1.get(%2)"
-  _get :: JSPtr p -> JSString -> IO a
+  _get :: JSPtr (BBModel a) -> JSString -> IO b
 
 foreign import jscript "%1.set(%2)"
-  set :: JSPtr p -> AnonObj -> IO ()
+  set :: JSPtr (BBModel a) -> AnonObj -> IO ()
 
 foreign import jscript "%1.set(%*)"
-  set' :: JSPtr p -> AnonObj -> AnonObj -> IO ()
+  set' :: JSPtr (BBModel a) -> AnonObj -> AnonObj -> IO ()
 
-escape :: JSPtr p -> String -> IO String
+escape :: JSPtr (BBModel a) -> String -> IO String
 escape p s = fromJSM $ _escape p (toJS s)
 
 foreign import jscript "%1.escape(%2)"
-  _escape :: JSPtr p -> JSString -> IO JSString
+  _escape :: JSPtr (BBModel a) -> JSString -> IO JSString
+
+has :: JSPtr (BBModel a) -> String -> IO Bool
+has p s = _has p (toJS s)
+
+foreign import jscript "%1.has(%2)"
+  _has :: JSPtr (BBModel a) -> JSString -> IO Bool
+
+unset :: JSPtr (BBModel a) -> String -> IO ()
+unset p s = _unset p (toJS s)
+
+foreign import jscript "%1.unset(%2)"
+  _unset :: JSPtr (BBModel a) -> JSString -> IO ()
+
+unset' :: JSPtr (BBModel a) -> String -> AnonObj -> IO ()
+unset' p s o = _unset' p (toJS s) o
+
+foreign import jscript "%1.unset(%*)"
+  _unset' :: JSPtr (BBModel a) -> JSString -> AnonObj -> IO ()
+
+foreign import jscript "%1.clear()"
+  clear :: JSPtr (BBModel a) -> IO ()
+
+foreign import jscript "%1.clear(%*)"
+  clear' :: JSPtr (BBModel a) -> AnonObj -> IO ()
+
+silentOpt :: IO AnonObj
+silentOpt = do
+  obj <- mkAnonObj
+  setAttr "silent" True obj
+  return obj
+
+modelID :: JSPtr (BBModel a) -> IO String
+modelID = getAttr "id"
+
+modelCID :: JSPtr (BBModel a) -> IO String
+modelCID = getAttr "cid"
+
+modelAttributes :: JSPtr (BBModel a) -> IO AnonObj
+modelAttributes = getAttr "attributes"
+
+-- TODO: defaults
+
+foreign import jscript "%1.toJSON()"
+  toJSON :: JSPtr (BBModel a) -> IO AnonObj
+
+foreign import jscript "%1.fetch()"
+  fetch :: JSPtr (BBModel a) -> IO ()
+
+foreign import jscript "%1.fetch(%*)"
+  fetch' :: JSPtr (BBModel a) -> AnonObj -> IO ()
+
+
+
 
