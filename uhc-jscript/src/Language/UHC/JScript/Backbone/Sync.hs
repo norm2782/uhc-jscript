6a9582f2444e3fdd09baa631cdd3d5f17f7603a7
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-19 15:10:18 +0200

    BB Sync

--- uhc-jscript/src/Language/UHC/JScript/Backbone/Sync.hs
+++ uhc-jscript/src/Language/UHC/JScript/Backbone/Sync.hs
@@ -1,2 +1,21 @@
 module Language.UHC.JScript.Backbone.Sync where
 
+import Language.UHC.JScript.Backbone.Model
+import Language.UHC.JScript.Primitives
+import Language.UHC.JScript.ECMA.String
+import Language.UHC.JScript.Types
+
+sync :: String -> BBModel a -> IO ()
+sync s = _sync (toJS s)
+
+foreign import jscript "Backbone.sync(%*)"
+  _sync :: JSString -> BBModel a -> IO ()
+
+sync' :: String -> BBModel a -> AnonObj -> IO ()
+sync' s = _sync' (toJS s)
+
+foreign import jscript "Backbone.sync(%*)"
+  _sync' :: JSString -> BBModel a -> AnonObj -> IO ()
+
+-- TODO: emulateHTTP
+-- TODO: emulateJSON
