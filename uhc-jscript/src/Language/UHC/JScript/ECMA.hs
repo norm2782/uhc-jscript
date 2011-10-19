f2bbe24a4c6ebf6c10b9c30588a7c613f34d4203
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-19 16:47:19 +0200

    Add ECMA module

---
+++ uhc-jscript/src/Language/UHC/JScript/ECMA.hs
@@ -0,0 +1,6 @@
+module Language.UHC.JScript.ECMA where
+
+import  Language.UHC.JScript.ECMA.Array
+import  Language.UHC.JScript.ECMA.Expressions
+import  Language.UHC.JScript.ECMA.String
+
--- uhc-jscript/src/Main.hs
+++ uhc-jscript/src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
+import  Language.UHC.JScript.Assorted
 import  Language.UHC.JScript.Backbone
-import  Language.UHC.JScript.ECMA.Array
-import  Language.UHC.JScript.ECMA.String
+import  Language.UHC.JScript.ECMA
 import  Language.UHC.JScript.JQuery.JQuery
 import  Language.UHC.JScript.JSON2.JSON2
 import  Language.UHC.JScript.Types
