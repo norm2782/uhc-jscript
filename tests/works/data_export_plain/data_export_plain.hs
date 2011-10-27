7a1ac478bc010d4ad0c2b7159fcacd1540fba077
Jurriën Stutterheim <j.stutterheim@me.com>
2011-10-27 16:03:26 +0200

    Add --no-hi-check to makefile

--- tests/works/data_export_plain/data_export_plain.hs
+++ tests/works/data_export_plain/data_export_plain.hs
@@ -1,14 +1,26 @@
+import Language.UHC.JScript.ECMA.String
+
 main = do
   putStrLn "data_export"
-  putStrLn $ show myBook
+  {- putStrLn $ show myBook-}
+
+data BookPtr
 
 data Book
   =  Book
-  {  title   :: String
-  ,  author  :: String
+  {  title   :: JSString
+  ,  author  :: JSString
   }
-  deriving Show
+  {- deriving Show-}
 
-myBook = Book "story" "me"
+add :: Int -> Int -> Int
+add x y = y + x
 
-foreign export jscript "myBookExp" myBook :: Book
+foreign export jscript "addFoo" add :: Int -> Int -> Int
+
+myBook = Book (stringToJSString "story") (stringToJSString "me")
+
+{- foreign export jscript "myBookExp" myBook :: Book-}
+foreign export jscript "{myBookExp}" myBook :: Book
+foreign import jscript "myBook" myBookPtr :: BookPtr
+
